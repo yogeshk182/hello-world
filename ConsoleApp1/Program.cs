493fc4bcfa6baa1ca6dfb26077c9e1046cde749d
Yogesh <Yogesh@PAVILION>
2022-11-07 11:29:22 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "yogesh";
+            string secondName = "j";
+            string name = string.Concat(firstName, secondName);
+            Console.WriteLine(name);
+        }
+    }
+}
